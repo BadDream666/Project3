0e87b7d3e8f1dafbf38355fd93a1f6ce89e6f070
Your Name <you@example.com>
2024-10-23 18:41:33 +0300

    project

---
+++ src/api_client.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+
+
+class APIClient(ABC):
+    """ Abstract API Client """
+
+    @abstractmethod
+    def get_info(self):
+        pass
---
+++ test/test_save_to_database.py
@@ -0,0 +1,5 @@
+from src.utils import save_to_database
+
+
+def test_save_to_database():
+    pass
